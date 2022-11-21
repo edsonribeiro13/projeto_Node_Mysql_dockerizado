2f21c57050382aa120f765708943d889464ded77
edsonribeiro13 <edson.viniciussr@gmail.com>
2022-11-21 01:48:20 -0300

    feat: login

---
+++ src/app/login.ts
@@ -0,0 +1,30 @@
+import userModel from '../repository/model/userModel'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+export default async (username: string, password: string) => {
+    const userFromDataBase = await userModel().findOne({
+        where: {
+            userName: username,
+        },
+    })
+    if (userFromDataBase?.dataValues.password) {
+        const hashResponse = await bcrypt
+            .compare(password, userFromDataBase.dataValues.password)
+            .then((res) => {
+                return res
+            })
+
+        if (hashResponse === true) {
+            const userName = userFromDataBase.dataValues.userName
+            const token = jwt.sign({ userName }, String(process.env.secret), {
+                expiresIn: 86400,
+            })
+            return token
+        } else {
+            return 'senha incorreta'
+        }
+    } else {
+        return 'usuário não encontrado'
+    }
+}
